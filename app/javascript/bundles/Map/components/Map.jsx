bb1f9bd807208cf0b81245e5a3cd18c98eb256ca
Jeremy Solomon <jeremysolomon2@gmail.com>
2018-10-09 18:36:06 -0400

    map can recenter on location now

--- app/javascript/bundles/Map/components/Map.jsx
+++ app/javascript/bundles/Map/components/Map.jsx
@@ -2,57 +2,63 @@ import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
  
 export default class Map extends Component {
-  componentDidMount() {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiamVyZW15c29sb21vbjIiLCJhIjoiY2puMjhndDQ3MDByZDNxbzRvNGdiOWduZiJ9.UWv2dTb0-kqL7ROiMuShPA'
-    let { coordinates } = this.props;
-    const mapOptions = {
-      container: this.mapContainer,
-      style: `mapbox://styles/mapbox/streets-v9`,
-      zoom: 12,
-      center: coordinates
-    };
-    const geolocationOptions = {
-        enableHighAccuracy: true,
-        maximumAge        : 30000,
-        timeout           : 27000
-      };
-    if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(
-        // success callback
-        (position) => {
-            coordinates = [
-                            position.coords.longitude,
-                            position.coords.latitude
-                          ];
-            document.getElementById("long")
-                    .innerHTML = coordinates[0];
-            document.getElementById("lat")
-                    .innerHTML = coordinates[1];
-            mapOptions.center = coordinates;
-            this.createMap(mapOptions);
-          },
-        // failure callback
-        () => { this.createMap(mapOptions) },
-        // options
-        geolocationOptions
-        );
-    }
-  }
-
-  createMap = mapOptions => {
+    componentDidMount() {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiYW5keXdlaXNzMTk4MiIsImEiOiJIeHpkYVBrIn0.3N03oecxx5TaQz7YLg2HqA'
+        let { coordinates } = this.props;
+        const mapOptions = {
+          container: this.mapContainer,
+          style: `mapbox://styles/mapbox/streets-v9`,
+          zoom: 12,
+          center: coordinates
+        };
+        const geolocationOptions = {
+          enableHighAccuracy: true,
+          maximumAge        : 30000,
+          timeout           : 27000
+        };
+        if ("geolocation" in navigator) {
+          navigator.geolocation.getCurrentPosition(
+            // success callback
+            (position) => {
+              coordinates = [
+                              position.coords.longitude,
+                              position.coords.latitude
+                            ];
+              document.getElementById("long")
+                      .innerHTML = coordinates[0];
+              document.getElementById("lat")
+                      .innerHTML = coordinates[1];
+              mapOptions.center = coordinates;
+              this.createMap(mapOptions, geolocationOptions);
+            },
+            // failure callback
+            () => { this.createMap(mapOptions, geolocationOptions) },
+            // options
+            geolocationOptions
+          );
+        }
+      }
+     
+    createMap = (mapOptions, geolocationOptions) => {
     this.map = new mapboxgl.Map(mapOptions);
     const map = this.map;
+    map.addControl(
+        new mapboxgl.GeolocateControl({
+        positionOptions: geolocationOptions,
+        trackUserLocation: true
+        })
+    );
     map.on('load', (event) => {
-      map.addSource(
+        map.addSource(
         'courts',
         {
-          type: 'geojson',
-          data: `/courts.json`
+            type: 'geojson',
+            data: `/courts.json`
         }
-      );
-      map.addLayer({ id: 'courts', type: 'circle', source: 'courts'});
+        );
+        map.addLayer({ id: 'courts', type: 'circle', source: 'courts'});
     });
-  }
+    }
 
   render() {
     const style = {
