edf8f56ef1d0dfff316073c7b39146f8ff8645ec
Jeremy Solomon <jeremysolomon2@gmail.com>
2018-10-10 21:07:28 -0400

    court and game show view

--- app/javascript/bundles/Game/components/Game.jsx
+++ app/javascript/bundles/Game/components/Game.jsx
@@ -4,8 +4,26 @@ import axios from 'axios';
  
 export default class Game extends Component {
 
+
+
+
+  fetchGame = (id) => {
+    axios.get(`games/${id}.json`)
+      .then((response) => {
+        this.setState({
+          term,
+          page:       response.data.page,
+          tasks:      response.data.tasks,
+          totalPages: response.data.total_pages
+        })
+      })
+  }
+
   render() {
-   return <h1>Game React Component</h1>
+   return (
+   <h1>Game React Component</h1>
+
+   )
   }
  
 }
