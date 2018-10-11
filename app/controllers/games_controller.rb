e3c3b3d537a56b52fa0ed1eeafdc303652d43900
Jeremy Solomon <jeremysolomon2@gmail.com>
2018-10-10 20:29:03 -0400

    game json working

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,23 @@
 class GamesController < ApplicationController
   
     def show
-      @court = Court.find(params[:id])
       @game = Game.find(params[:id])
+      @court = @game.court
+      @user = @game.user
+      @scorekeeper = @game.score_keeper
+      @opponent = @game.opponent
+
+      respond_to do |format|
+        format.html
+        format.json do
+          render json: { 
+              game: @game,
+              user: @user,
+              score_keeper: @score_keeper,
+              opponent: @opponent
+            }
+        end
+      end
     end
   
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
   has_many :games
   has_many :games_where_opponent, class_name: "Game",  foreign_key: "opponent_id"
   has_many :games_where_keeper, class_name: "Game",  foreign_key: "score_keeper_id"
+
+  def self.get_nickname(user_id)
+    User.find(user_id).nickname
+  end
 end
